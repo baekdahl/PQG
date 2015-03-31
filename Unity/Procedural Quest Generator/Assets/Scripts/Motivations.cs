ed724da8be9f0ff08ebbfe019ed6b05080abdf3f
Lystlund <raslystlund@gmail.com>
2015-04-01 00:05:16 +0200

    Motivation
    Started working on Motication script

---
+++ Unity/Procedural Quest Generator/Assets/Scripts/Motivations.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Motivations : MonoBehaviour {
+	enum Knowledge{
+
+	}
+
+	enum Motivation {
+		//Knowledge, Comfort, Reputation, Serenity, Protection, Conquest, Wealth, Ability, Equipment
+	}
+
+	private void Start(){
+
+	}
+}
